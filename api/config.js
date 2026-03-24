236d3b210047766daf230b172ca2c73efa26043b
patoaceves <pgzzaceves@gmail.com>
2026-03-24 15:54:39 -0600

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,10 @@
+// api/config.js
+// Endpoint que devuelve los tokens como JSON seguro
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-store');
+  res.status(200).json({
+    tokenParticipantes: process.env.QUERY_ID_PARTICIPANTE || '',
+    tokenEvaluaciones:  process.env.EVALUACION_FELICIDAD  || ''
+  });
+};
